1229c2c01c3e6fdf652e8600aa61885fec4711f9
Programazion <programazion1@gmail.com>
2023-07-25 11:32:07 -0500

    main

---
+++ script.js
@@ -0,0 +1,10 @@
+function suma(){
+    let a = document.getElementById('numero-a')
+    let b = document.getElementById('numero-b')
+    resultado = a+b
+    return resultado
+    console.log(resultado)
+}
+
+
+suma(resultado)
