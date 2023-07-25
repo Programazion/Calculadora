256d6675b1274246933dee3fb0ae24db56e56f6a
Programazion <programazion1@gmail.com>
2023-07-25 12:02:43 -0500

    Frontend html-Css - Functions js

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
+console.log('Loading Javascript');
+
 function suma(){
     let a = document.getElementById('numero-a')
     let b = document.getElementById('numero-b')
-    resultado = a+b
-    return resultado
-    console.log(resultado)
+    let r = a+b;
+    return r
 }
+console.log(resultado);
 
-
-suma(resultado)
+/*  */
+/* suma(resultado); */
+/*  */
+/* function resta(a,b){ */
+/*     let a = document.getElementById('numero-a') */
+/*     let b = document.getElementById('numero-b') */
+/*     let rest = a-b */
+/*     return rest */
+/*     console.log(rest); */
+/* } */
+/* resta(rest); */
+/*  */
+/* function division(a,b){ */
+/*     let a = document.getElementById('numero-a') */
+/*     let b = document.getElementById('numero-b') */
+/*     let div = a/b */
+/*     return (div); */
+/*     console.log(div); */
+/* } */
+/* division(div); */
+/*  */
+/* function multiplicacion(a,b){ */
+/*     let a = document.getElementById('numero-a') */
+/*     let b = document.getElementById('numero-b') */
+/*     let mult = a/b */
+/*     return (mult); */
+/*     console.log(mult);     */
+/* } */
+/* multiplicacion(mult); */
