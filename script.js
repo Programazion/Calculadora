12aa37c49b89a035325b84f715c69366a102895f
Programazion <programazion1@gmail.com>
2023-07-26 01:23:16 -0500

    Final commit

--- script.js
+++ script.js
@@ -1,39 +1,61 @@
 console.log('Loading Javascript');
 
-function suma(){
-    let a = document.getElementById('numero-a')
-    let b = document.getElementById('numero-b')
-    let r = a+b;
-    return r
-}
-console.log(resultado);
+const valorA = document.getElementById('numeroA').value
 
-/*  */
-/* suma(resultado); */
-/*  */
-/* function resta(a,b){ */
-/*     let a = document.getElementById('numero-a') */
-/*     let b = document.getElementById('numero-b') */
-/*     let rest = a-b */
-/*     return rest */
-/*     console.log(rest); */
-/* } */
-/* resta(rest); */
-/*  */
-/* function division(a,b){ */
-/*     let a = document.getElementById('numero-a') */
-/*     let b = document.getElementById('numero-b') */
-/*     let div = a/b */
-/*     return (div); */
-/*     console.log(div); */
-/* } */
-/* division(div); */
-/*  */
-/* function multiplicacion(a,b){ */
-/*     let a = document.getElementById('numero-a') */
-/*     let b = document.getElementById('numero-b') */
-/*     let mult = a/b */
-/*     return (mult); */
-/*     console.log(mult);     */
-/* } */
-/* multiplicacion(mult); */
+console.log('Valor A =>', valorA)
+
+function suma(){ 
+    console.log('sumando')
+    const valorA = document.getElementById('numeroA').value;
+    const valorB = document.getElementById('numeroB').value;
+    console.log('Valor ingresado A=>',valorA);
+    console.log('Valor ingresado B =>',valorB);
+//Es importante declarar la variable number para quepueda hacer operaciones
+    const resultado= Number(valorA)+Number(valorB);
+    console.log('El resultado es', resultado);
+//Variable que captura suma 
+    let r= document.getElementById('resultado');
+    r.value=resultado
+}
+
+function resta(){ 
+    console.log('restando')
+    const valorA = document.getElementById('numeroA').value;
+    const valorB = document.getElementById('numeroB').value;
+    console.log('Valor ingresado A=>',valorA);
+    console.log('Valor ingresado B =>',valorB);
+//Es importante declarar la variable number para quepueda hacer operaciones
+    const resultado= Number(valorA)-Number(valorB);
+    console.log('El resultado es', resultado);
+//Variable que captura datos 
+    let r= document.getElementById('resultado');
+    r.value=resultado
+}
+
+function division(){ 
+    console.log('dividiendo')
+    const valorA = document.getElementById('numeroA').value;
+    const valorB = document.getElementById('numeroB').value;
+    console.log('Valor ingresado A=>',valorA);
+    console.log('Valor ingresado B =>',valorB);
+//Es importante declarar la variable number para que pueda operar
+    const resultado= Number(valorA)/Number(valorB);
+    console.log('El resultado es', resultado);
+//Variable que captura datos 
+    let r= document.getElementById('resultado');
+    r.value=resultado
+}
+
+function multiplicar(){ 
+    console.log('multiplicando')
+    const valorA = document.getElementById('numeroA').value;
+    const valorB = document.getElementById('numeroB').value;
+    console.log('Valor ingresado A=>',valorA);
+    console.log('Valor ingresado B =>',valorB);
+//Es importante declarar la variable number para que pueda operar
+    const resultado= Number(valorA)*Number(valorB);
+    console.log('El resultado es', resultado);
+//Variable que captura datos 
+    let r= document.getElementById('resultado');
+    r.value=resultado
+}
